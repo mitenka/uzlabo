3abe0fd7fd502d5fe4f26a42555be7e711c4a1ef
Dmitry Zhuravlev-Nevsky <dmitry.nevsky@gmail.com>
2021-01-11 16:14:31 +0200

    Add About Us page

--- uzlabo/urls.py
+++ uzlabo/urls.py
@@ -8,12 +8,12 @@ from pages import views as pages_views
 
 urlpatterns = i18n_patterns(
     path('', pages_views.home, name='pages-home'),
+    path('about/', pages_views.about, name='pages-about'),
 
     path('ideas/', ideas_views.index, name='ideas-index'),
     path('ideas/<int:pk>/', ideas_views.details, name='ideas-details'),
     path('ideas/new/', ideas_views.new, name='ideas-new'),
 
-    path('pages/', include('django.contrib.flatpages.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
